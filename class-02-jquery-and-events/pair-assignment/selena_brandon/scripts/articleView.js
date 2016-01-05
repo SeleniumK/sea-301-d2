fe5933adc1d33c75c4ae608a33f020b6507ecc0d
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-05 15:26:56 -0800

    second handleauthorfilter TODO completed.

--- class-02-jquery-and-events/pair-assignment/selena_brandon/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/selena_brandon/scripts/articleView.js
@@ -31,18 +31,14 @@ articleView.handleAuthorFilter = function() {
     if ($(this).val()) {
       $('article').hide();
       $('article:not(.template)').each(function(){
-        console.log($(this).data('author'));
-        // console.log($('#author-filter').val);
-
         if ($(this).data('author') === $('#author-filter option:selected').text()){
           $(this).show();
         }
       });
-      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
-      //       and then show just the ones that match for the author that was selected.
-      //       Use an "attribute selector" to find those articles, and fade them in for the reader.
 
     } else {
+        $('article:not(.template)').show();
+      
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
 
