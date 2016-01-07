406637481db11a86d4ee33dea81546f1c42d6342
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-01-07 13:48:04 -0800

    Todos from January 7, Collaboration between Selena and Adam. Set up handlebar, created template, dynamically populated page with articles

---
+++ class-04-templates/pair-assignment/selena-adam/starter-code/scripts/article.js
@@ -0,0 +1,31 @@
+var articles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
+}
+
+Article.prototype.toHtml = function(a) {
+  var source = $('#template').html();
+  var template = Handlebars.compile(source);
+  var html = template(a);
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  return html;
+};
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(ele) {
+  articles.push(new Article(ele));
+})
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml(a));
+});
