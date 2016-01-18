6e99e865b41f437340a9b5e78cabb13b4f2b7dee
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-01-18 13:34:20 -0800

    Pair assignment 1/18 with Raziyeh, Willy, and Selena. Added Page.js, put in routes, and got rid of handleMainNav. Page now handles redirects based on the click, without reloading pages

--- class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/articleController.js
@@ -1,13 +1,11 @@
 (function(module) {
   var articlesController = {};
 
-  // TODO: Create the `articles` table when the controller first loads, with the code that used to be in index.html:
-
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
-  // code that used to be in index.html.
-  // Also be sure to hide all the main section elements, and reveal the #articles section:
   articlesController.index = function() {
-
+    $('#about').hide();
+    $('#articles').show();
+    Article.createTable();
+    Article.fetchAll(articleView.initIndexPage);
   };
 
   module.articlesController = articlesController;
--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,13 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
-// linked to a a single controller function to handle it:
+page.base('/');
+page('/', index);
+page('/about', about);
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+page();
+
+function index(){
+  articlesController.index();
+}
+
+function about(){
+  aboutController.index();
+}
